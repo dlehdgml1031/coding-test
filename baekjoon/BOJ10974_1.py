e52674761bed5ce1f8df48731f9237079570f1f7
dlehdgml1031 <dlehdgml1031@gmail.com>
2025-12-07 22:46:00 +0900

    BOJ10974

---
+++ baekjoon/BOJ10974_1.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = [i for i in range(1, n + 1)]
+check = [False] * len(arr)
+choose = []
+
+def permutation(level):
+    if level == n:
+        print(*choose)
+        return
+    
+    for i in range(0, len(arr)):
+        if check[i] == True:
+            continue
+        choose.append(arr[i])
+        check[i] = True
+        
+        permutation(level + 1)
+        
+        check[i] = False
+        choose.pop()
+        
+permutation(0)
